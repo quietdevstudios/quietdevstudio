c0f55c84a4babbe0db3ca4bb6784d94a464159cb
quietdevstudios <quietdevstudio@gmail.com>
2024-11-08 18:55:14 +0100

    nothing changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
       <Header />
-      <h1 className='text-center text-3xl text-orange-500 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+      <h1 className="flex items-center justify-center text-3xl text-orange-600">
         We're Launching Soon...
       </h1>
+      {/* <h1 className="text-center text-3xl text-orange-950 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        We're Launching Soon...
+      </h1> */}
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App 
+export default App;
