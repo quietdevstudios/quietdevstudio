50b5e289edc94fd2bab22ff4e49f24fe2c35b828
quietdevstudios <quietdevstudio@gmail.com>
2024-11-08 18:54:27 +0100

    created data js file to be able to work with multiple data at once in one place

---
+++ data.js
@@ -0,0 +1,26 @@
+export const NAV_LINKS = [
+  {
+    linkText: "All Path",
+    hyperLink: "path",
+  },
+  {
+    linkText: "About",
+    hyperLink: "about",
+  },
+  {
+    linkText: "Community",
+    hyperLink: "community",
+  },
+  {
+    linkText: "Support us",
+    hyperLink: "support",
+  },
+  {
+    linkText: "Sign in",
+    hyperLink: "sign-in",
+  },
+  {
+    linkText: "Get Started",
+    hyperLink: "sign-up",
+  },
+];
