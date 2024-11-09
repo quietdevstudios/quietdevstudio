7012b2a65ac8cd6a2bbddf596ce67e89b5ef6d98
quietdevstudios <quietdevstudio@gmail.com>
2024-11-09 16:43:44 +0100

    changed image on website

--- src/components/Maintenance.jsx
+++ src/components/Maintenance.jsx
@@ -4,8 +4,12 @@ import maintenanceImg from "/assets/images/bulb.png";
 const Maintenance = () => {
   return (
     <>
-      <section className="bg-orange-50">
+      <section className="bg-orange-50 flex flex-col items-center justify-center min-h-screen stars">
+        <h2 className="text-4xl text-orange-600 font-semibold">Website is</h2>
         <img src={maintenanceImg} className="w-full max-w-7xl mx-auto" />
+        <h2 className="text-4xl text-orange-600 font-semibold">
+          Will be back soon.
+        </h2>
       </section>
     </>
   );
