77653997ce9013637976024cfdbdf4e84a823e8b
quietdevstudios <quietdevstudio@gmail.com>
2024-11-09 16:46:02 +0100

    changes made

--- src/components/Maintenance.jsx
+++ src/components/Maintenance.jsx
@@ -4,10 +4,10 @@ import maintenanceImg from "/assets/images/bulb.png";
 const Maintenance = () => {
   return (
     <>
-      <section className="bg-orange-50 flex flex-col items-center justify-center min-h-screen stars">
-        <h2 className="text-4xl text-orange-600 font-semibold">Website is</h2>
+      <section className="bg-orange-0 flex flex-col items-center justify-center min-h-screen stars">
+        <h2 className="text-4xl text-orange-500 font-semibold">Website is</h2>
         <img src={maintenanceImg} className="w-full max-w-7xl mx-auto" />
-        <h2 className="text-4xl text-orange-600 font-semibold">
+        <h2 className="text-4xl text-orange-500 font-semibold">
           Will be back soon.
         </h2>
       </section>
