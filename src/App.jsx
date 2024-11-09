068bd49f2092bf94899731c7e42c6a14d4b5ac7b
quietdevstudios <quietdevstudio@gmail.com>
2024-11-09 10:31:19 +0100

    error handling

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,10 @@ const App = () => {
   return (
     <>
       <Header />
-      <h1 className="flex items-center justify-center text-3xl text-orange-600">
-        We're Launching Soon...
-      </h1>
-      {/* <h1 className="text-center text-3xl text-orange-950 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      {/* <h1 className="flex items-center justify-center text-3xl text-orange-600">
         We're Launching Soon...
       </h1> */}
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 };
