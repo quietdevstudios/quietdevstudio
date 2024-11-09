10cc02f27ef58645b52923dd2ee333060cca9403
quietdevstudios <quietdevstudio@gmail.com>
2024-11-09 16:53:18 +0100

    added width and mx-auto

--- src/components/Maintenance.jsx
+++ src/components/Maintenance.jsx
@@ -4,7 +4,7 @@ import maintenanceImg from "/assets/images/bulb.png";
 const Maintenance = () => {
   return (
     <>
-      <section className="bg-orange-0 flex flex-col items-center justify-center min-h-screen my-10 stars">
+      <section className="bg-orange-0 flex flex-col items-center justify-center min-h-screen my-10 w-[95%] mx-auto stars">
         <h2 className="text-4xl text-orange-500 font-semibold">Website is</h2>
         <img src={maintenanceImg} className="w-full max-w-7xl mx-auto" />
         <h2 className="text-4xl text-orange-500 font-semibold">
