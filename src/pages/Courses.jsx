e00fddfc802e65790f52bf9a7028561245c13456
unknown <TL@ARMP-SUR-IT101.access-armpensions.com>
2025-01-03 16:37:21 +0100

    creating course modules

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -6,12 +6,12 @@ import { faGraduationCap } from "@fortawesome/free-solid-svg-icons";
 const Courses = () => {
   return (
     <section>
-      <article className="relative">
+      <article className="relative bg-orange-100">
         <header className="p-6">
           <h1 className="text-[2rem] font-medium tracking-tighter text-orange-600">
             Quietdev Studios Curriculum
           </h1>
-          <h4 className="text-[1.5rem] font-medium tracking-tight">
+          <h4 className="text-[1.5rem] font-medium tracking-tight leading-7 mt-2">
             The essential skillset for new front-end developers
           </h4>
           <p className="mt-4 font-regular text-lg">
@@ -27,7 +27,7 @@ const Courses = () => {
         <figure>
           <img src={vector} alt="vector image" />
         </figure>
-        <section className="flex items-center justify-evenly bg-orange-400 w-[80%] md:max-w-[60%] mx-auto p-6 absolute -bottom-5 left-0 right-0 rounded-md text-white font-medium text-2xl tracking-tighter">
+        <section className="flex items-center justify-evenly bg-orange-400 w-[80%] md:max-w-[60%] mx-auto p-6 absolute -bottom-14 left-0 right-0 rounded-md text-white font-medium text-2xl tracking-tighter">
           <div>
             {/* <FontAwesomeIcon icon={faGraduationCap} /> */}
             <p>Beginner's level</p>
@@ -37,6 +37,41 @@ const Courses = () => {
           </div>
         </section>
       </article>
+
+      {/* about the curriculum */}
+      <section>
+        <h1 className="text-[2rem] font-medium tracking-tighter text-orange-600 mt-28 px-6">
+          About the curriculum
+        </h1>
+        <div className="flex flex-col gap-4 text-lg font-regular tracking-tight px-16 my-10">
+          <div>
+            <p>
+              Defines the essential skills and knowledge every front-end
+              developer needs for career success and industry relevance.
+            </p>
+          </div>
+          <div>
+            <p>
+              Created by Quietdev and refined with insights from students,
+              educators, and developers from the broader web community online.
+            </p>
+          </div>
+          <div>
+            <p>
+              Includes learning resource recommendations covering every
+              curriculum topic, helping you become job-ready.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <h1 className="text-[2rem] font-medium tracking-tighter text-orange-600 mt-12 px-6">
+          Modules
+        </h1>
+        <p>coming soon...</p>
+      </section>
+      
     </section>
   );
 };
