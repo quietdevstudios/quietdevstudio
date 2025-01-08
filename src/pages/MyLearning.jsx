408754334d25b56b1c97e0018f3e0fb1c9d5d0d8
unknown <TL@ARMP-SUR-IT101.access-armpensions.com>
2025-01-08 17:31:51 +0100

    list of courses feature added - DONE

--- src/pages/MyLearning.jsx
+++ src/pages/MyLearning.jsx
@@ -3,6 +3,11 @@ import { Link } from "react-router";
 import thumbnailImg from "/download.jpg";
 import cssImg from "/css.jpg";
 import jsImg from "/js.jpeg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 const MyLearning = () => {
   return (
@@ -45,7 +50,7 @@ const MyLearning = () => {
         <>
           {/* 1 */}
           <section className="flex flex-col gap-10 p-8">
-            <article className="leading-6s w-full p-2">
+            <article className="leading-6s w-full p-2 cursor-pointer">
               <img
                 src={thumbnailImg}
                 alt="course thumbnail"
@@ -102,6 +107,43 @@ const MyLearning = () => {
             </article>
           </section>
         </>
+        {/* prev/next */}
+        <section className="p-2">
+          <div className="flex items-center justify-center gap-4 py-2">
+            <FontAwesomeIcon
+              icon={faChevronLeft}
+              className="border border-orange-300 p-3 rounded-full w-4 h-4 cursor-pointer"
+            />
+            <ul className="flex gap-6 p-2 text-zinc-600">
+              <li>
+                <a
+                  href="#"
+                  className="border-b-[.2rem] border-orange-600 px-2 py-1"
+                >
+                  1
+                </a>
+              </li>
+              <li>
+                <a href="#">2</a>
+              </li>
+              <li>
+                <a href="#">3</a>
+              </li>
+              <li>
+                <a href="#">4</a>
+              </li>
+            </ul>
+            <FontAwesomeIcon
+              icon={faChevronRight}
+              className="border border-orange-300 p-3 rounded-full w-4 h-4 cursor-pointer"
+            />
+          </div>
+          <div>
+            <p className="text-center tracking-wider text-sm text-zinc-600">
+              1-3 of 10 courses
+            </p>
+          </div>
+        </section>
       </main>
     </section>
   );
