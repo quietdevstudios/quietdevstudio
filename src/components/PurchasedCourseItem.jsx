98fd2119905dd66e64a30720a688cdca3443a12e
quietdevstudios <quietdevstudio@gmail.com>
2025-01-16 11:06:25 +0100

    added dropdown feature for sections respectively

--- src/components/PurchasedCourseItem.jsx
+++ src/components/PurchasedCourseItem.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import video from "/wontFail.mp4";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -9,7 +9,14 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { COURSECONTENT } from "../../data";
 
-const PurchasedCourseItem = ({}) => {
+const PurchasedCourseItem = () => {
+  const [showContent, setShowContent] = useState(false);
+  const handleShowContent = (contentId) => {
+    setShowContent((prevContent) =>
+      prevContent === contentId ? null : contentId
+    );
+  };
+
   return (
     <main className="text-center font-montserrat">
       <video
@@ -41,33 +48,44 @@ const PurchasedCourseItem = ({}) => {
               sectionNumber,
               totalSteps,
             } = coursecontentitem;
+            const isVisible = showContent === id;
             return (
               <Fragment key={id}>
-                <button className="bg-zinc-100 py-6 px-4 text-left font-semibold text-[1rem] tracking-wide w-full border-t-2 border-zinc-400">
+                <button
+                  onClick={() => handleShowContent(id)}
+                  className={`bg-zinc-100 py-6 px-4 text-left font-semibold text-[1rem] tracking-wide w-full border-t-2 border-zinc-400`}
+                >
                   <span className="flex items-center justify-between">
                     <h2>
                       Section {sectionNumber}: {courseTitle}
                     </h2>
-                    <FontAwesomeIcon icon={faChevronDown} size="sm" />
+                    <FontAwesomeIcon
+                      icon={faChevronDown}
+                      size="sm"
+                      className={`transform transition-transform
+                        ${isVisible && "rotate-180"}`}
+                    />
                   </span>
                   <span className="text-xs font-medium">
                     {completedSteps} / {totalSteps} | {estimatedTime}
                   </span>
                 </button>
-                <li className="flex items-start gap-3 py-3">
-                  <div>
-                    <input type="checkbox" />
-                  </div>
-                  <div className="w-full flex flex-col gap-1">
-                    <h2 className="text-left">
-                      {sectionNumber}. {courseTitle}
-                    </h2>
-                    <span className="text-xs font-regular flex gap-2 items-center">
-                      <FontAwesomeIcon icon={faTv} />
-                      {estimatedTime}
-                    </span>
-                  </div>
-                </li>
+                {isVisible && (
+                  <li className="flex items-start gap-3 py-3">
+                    <div>
+                      <input type="checkbox" />
+                    </div>
+                    <div className="w-full flex flex-col gap-1">
+                      <h2 className="text-left">
+                        {sectionNumber}. {courseTitle}
+                      </h2>
+                      <span className="text-xs font-regular flex gap-2 items-center">
+                        <FontAwesomeIcon icon={faTv} />
+                        {estimatedTime}
+                      </span>
+                    </div>
+                  </li>
+                )}
               </Fragment>
             );
           })}
