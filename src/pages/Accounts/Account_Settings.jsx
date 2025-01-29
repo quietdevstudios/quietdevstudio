15224af1658662c4d9f827c615214b95f631cb75
quietdevstudios <quietdevstudio@gmail.com>
2025-01-29 14:43:26 +0100

    updates

--- src/pages/Accounts/Account_Settings.jsx
+++ src/pages/Accounts/Account_Settings.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Account_Settings = () => {
   return (
-    <div className="text-center p-8 font-medium tracking-wide uppercase">
+    <div className="text-center font-medium tracking-wide uppercase h-[50vh] flex items-center justify-center text-xl">
       Account_Settings
     </div>
   );
